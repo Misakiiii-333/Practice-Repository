ff6b72b6463d225b26d0c9302cb137a96305afd5
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-07 17:34:43 +0900

    Update 確認問題2.php

--- PHP-練習/確認問題2.php
+++ PHP-練習/確認問題2.php
@@ -1,8 +1,10 @@
 <?php
 
-function cal($a,$b,$c){
-    return ($a + $b + $c) /3 ;
-};
+function cal($a,$b,$c) {
+    return ($a + $b + $c) / 3 ;
+}
 
 $result = cal(9,4,2);
 echo $result."\n";
+
+?>
