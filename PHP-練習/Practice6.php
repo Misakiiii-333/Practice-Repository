320346c1ccefc9540fc7373778f360746ae35142
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-07 17:27:20 +0900

    Update Practice6.php

--- PHP-練習/Practice6.php
+++ PHP-練習/Practice6.php
@@ -1,10 +1,14 @@
 <?php
 $age= 0;
 
-if($age >=20){
- echo("adult");
-}else if ($age==0){
- echo("baby");
-}else{
+if($age >=20) {
+echo("adult");
+ 
+} elseif ($age==0) {
+echo("baby");
+ 
+} else {
  echo ("child");
 }
+
+?>
